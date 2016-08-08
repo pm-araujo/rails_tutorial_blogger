b0d79953e270f45b239d789f7b1b4afbf737f0f2
sight <sight.pedro@gmail.com>
2016-08-08 19:08:32 +0100

    reverting back to last blog_jfsg commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,6 @@ class ArticlesController < ApplicationController
 		@comment = Comment.new
 		@comment.article_id = @article.id
 		
-		Article.increment_counter(:view_count, @article.id)
 		@article.save	
 	end
 
@@ -21,7 +20,6 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(articles_params)
-		@article[:view_count] = 0
 		@article.save
 		flash.notice = "Article '#{@article.title}' created!"
 		redirect_to article_path(@article)
